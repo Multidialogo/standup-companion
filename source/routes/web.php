99cc9900743e89737734ee0ffb73ac64795def6d
Emidio Cruciani <cruciani@multidialogo.it>
2022-03-18 18:48:26 +0100

    SC/AUTH/BREEZE-WEMZ3W-Auth functionality + updated docs/local-provisioning.md

--- source/routes/web.php
+++ source/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::redirect('/dashboard', '/');
+
+require __DIR__.'/auth.php';
